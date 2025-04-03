388881fa495ef02b2593f81de1d45703fd611eee
Christian Murillo <christianoroceomurillo@gmail.com>
2025-04-04 01:16:39 +0800

    Implemented validation of expired refresh token

--- src/api/controllers/token.controller.ts
+++ src/api/controllers/token.controller.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { apiResponse } from "../../shared/utils/api-response";
-import { MESSAGE_DATA_INVALID_TOKEN, MESSAGE_DATA_NOT_EXIST, MESSAGE_DATA_REFRESH_TOKEN } from "../../shared/constants/message.constant";
+import { MESSAGE_DATA_INVALID_TOKEN, MESSAGE_DATA_NOT_EXIST, MESSAGE_DATA_REFRESH_TOKEN, MESSAGE_DATA_TOKEN_EXPIRED } from "../../shared/constants/message.constant";
 import { ERROR_ON_REFRESH_TOKEN } from "../../shared/constants/error.constant";
 import { token as validator } from "../../middlewares/validators/authentications.validator";
 import BadRequestException from "../../shared/exceptions/bad-request.exception";
@@ -51,6 +51,14 @@ const controller = async (
 
     if (session.access_token !== token) {
       throw new UnauthorizedException([MESSAGE_DATA_INVALID_TOKEN]);
+    }
+
+    // Validate if refresh token date was expired
+    const refreshTokenExpiryDate = new Date(session.refresh_token_expires_at);
+    const currentDate = new Date();
+    if (refreshTokenExpiryDate < currentDate) {
+      await sessionService.delete(session.id!);
+      throw new UnauthorizedException([MESSAGE_DATA_TOKEN_EXPIRED]);
     }
 
     const record = await service.getById(tokenData.id as unknown as string);

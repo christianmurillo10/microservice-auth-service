db3ef1aa7f251ec91feec1099d923bcc379eff1e
Christian Murillo <christianoroceomurillo@gmail.com>
2025-05-31 17:20:15 +0800

    Refactor token service

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,7 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
-import { UsersAccessType } from "../entities/users.entity";
+import { UsersAccessType, UsersAccessTypeValue } from "../entities/users.entity";
 import { MESSAGE_DATA_INVALID_TOKEN, MESSAGE_DATA_TOKEN_EXPIRED } from "../shared/constants/message.constant";
 import UnauthorizedException from "../shared/exceptions/unauthorized.exception";
+import NotFoundException from "../shared/exceptions/not-found.exception";
 import { addDaysToDate, addMinutesToDate } from "../shared/helpers/common.helper";
 import { generateAccessToken, verifyToken } from "../shared/helpers/jwt.helper";
 import SessionsService from "./sessions.service";
@@ -40,6 +41,33 @@ export default class TokenService {
     }
   };
 
+  private getUser = async (id: string) => this.usersService
+    .getById(id)
+    .catch(err => {
+      if (err instanceof NotFoundException) {
+        throw new UnauthorizedException([MESSAGE_DATA_INVALID_TOKEN]);
+      }
+
+      throw err;
+    });
+
+  private getAccessToken = (
+    id: number,
+    email: string,
+    accessType: UsersAccessTypeValue,
+    subject: number,
+  ) => {
+    const accessTokenExpiryDate = addMinutesToDate(new Date(), 30);
+    const accessTokenExpiry = accessTokenExpiryDate.getTime() / 1000;
+    const accessToken = generateAccessToken(
+      id,
+      email,
+      accessType,
+      subject,
+      accessTokenExpiry
+    );
+    return { accessTokenExpiryDate, accessToken };
+  };
   execute = async (): Promise<Output> => {
     const tokenData = verifyToken(this.input.token);
 
@@ -60,15 +88,12 @@ export default class TokenService {
     await this.validateRefreshToken(session);
 
     // Generate Access Token
-    const record = await this.usersService.getById(tokenData.id as unknown as string);
-    const accessTokenExpiryDate = addMinutesToDate(new Date(), 30);
-    const accessTokenExpiry = accessTokenExpiryDate.getTime() / 1000;
-    const accessToken = generateAccessToken(
+    const record = await this.getUser(tokenData.id as unknown as string);
+    const { accessTokenExpiryDate, accessToken } = this.getAccessToken(
       record.id as unknown as number,
       record.email,
       tokenData.client,
       record.business_id as unknown as number,
-      accessTokenExpiry
     );
 
     // Save data to sessions table

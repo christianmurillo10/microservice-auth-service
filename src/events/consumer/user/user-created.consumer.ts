93f008ec55e6336dc1b47b00e3cd2d300e383bf9
Christian Murillo <christianoroceomurillo@gmail.com>
2025-06-03 17:30:17 +0800

    Revert changes and fixed capturing of data

--- src/events/consumer/user/user-created.consumer.ts
+++ src/events/consumer/user/user-created.consumer.ts
@@ -5,13 +5,25 @@ import UsersService from "../../../services/users.service";
 const usersService = new UsersService();
 
 const subscribeUserCreated = async (message: Message): Promise<void> => {
-  const value: UsersModel = JSON.parse(message.value?.toString() ?? '{}');
-  const userId = value.id!;
-  await usersService.save(value)
+  const value = JSON.parse(message.value?.toString() ?? '{}');
+  const data = {
+    id: value.new_details.id,
+    name: value.new_details.name,
+    username: value.new_details.username,
+    email: value.new_details.email,
+    password: value.new_details.password,
+    access_type: value.new_details.access_type,
+    business_id: value.new_details.business_id,
+    is_active: value.new_details.is_active,
+    created_at: value.new_details.created_at,
+    updated_at: value.new_details.updated_at,
+  } as UsersModel;
+
+  await usersService.save(data)
     .catch(err => {
       console.log("Error on creating users", err);
     });
-  console.info(`Event Notification: Successfully created user ${userId}.`);
+  console.info(`Event Notification: Successfully created user ${data.id}.`);
 };
 
 export default subscribeUserCreated;
--- src/events/consumer/user/user-updated.consumer.ts
+++ src/events/consumer/user/user-updated.consumer.ts
@@ -6,8 +6,8 @@ import NotFoundException from "../../../shared/exceptions/not-found.exception";
 const usersService = new UsersService();
 
 const subscribeUserUpdated = async (message: Message): Promise<void> => {
-  const value: UsersModel = JSON.parse(message.value?.toString() ?? '{}');
-  const userId = value.id!;
+  const value = JSON.parse(message.value?.toString() ?? '{}');
+  const userId = value.new_details.id;
   const record = await usersService.getById(userId)
     .catch(err => {
       if (err instanceof NotFoundException) {
@@ -22,14 +22,25 @@ const subscribeUserUpdated = async (message: Message): Promise<void> => {
     return;
   }
 
-  await usersService.save({
+  const data = {
     ...record,
-    ...value,
-  })
+    id: value.new_details.id,
+    name: value.new_details.name,
+    username: value.new_details.username,
+    email: value.new_details.email,
+    password: value.new_details.password,
+    access_type: value.new_details.access_type,
+    is_active: value.new_details.is_active,
+    business_id: value.new_details.business_id,
+    created_at: value.new_details.created_at,
+    updated_at: value.new_details.updated_at,
+  } as UsersModel;
+
+  await usersService.save(data)
     .catch(err => {
       console.log("Error on updating users", err);
     });
-  console.info(`Event Notification: Successfully updated user ${userId}.`);
+  console.info(`Event Notification: Successfully updated user ${data.id}.`);
 };
 
 export default subscribeUserUpdated;

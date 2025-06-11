b4e9dc066666168b6cc610fc2c2d13a63f2c8207
Christian Murillo <christianoroceomurillo@gmail.com>
2025-06-11 20:02:05 +0800

    Updates for user consumer

--- src/events/consumer/user/index.ts
+++ src/events/consumer/user/index.ts
@@ -3,11 +3,13 @@ import {
   EVENT_USER_CREATED,
   EVENT_USER_UPDATED,
   EVENT_USER_DELETED,
-  EVENT_USER_PASSWORD_CHANGED
+  EVENT_USER_PASSWORD_CHANGED,
+  EVENT_USER_BULK_DELETED
 } from "../../../shared/constants/events.constant";
 import subscribeUserCreated from "./user-created.consumer";
 import subscribeUserUpdated from "./user-updated.consumer";
 import subscribeUserDeleted from "./user-deleted.consumer";
+import subscribeUserBulkDeleted from "./user-bulk-deleted.consumer";
 import subscribeUserPasswordChanged from "./user-password-changed.consumer";
 
 const userConsumer = async (message: KafkaMessage) => {
@@ -26,6 +28,9 @@ const userConsumer = async (message: KafkaMessage) => {
       break;
     case EVENT_USER_DELETED:
       await subscribeUserDeleted(value.data);
+      break;
+    case EVENT_USER_BULK_DELETED:
+      await subscribeUserBulkDeleted(value.data);
       break;
     case EVENT_USER_PASSWORD_CHANGED:
       await subscribeUserPasswordChanged(value.data);

5f4da571626c7fdd6a559bfd9b454013b9ff651a
Christian Murillo <christianoroceomurillo@gmail.com>
2025-08-22 22:58:38 +0800

    Implemented rebuild of user permission cache when updating user-permissions data

--- src/api/controllers/userPermission/delete.controller.ts
+++ src/api/controllers/userPermission/delete.controller.ts
@@ -3,6 +3,7 @@ import { apiResponse } from "../../../shared/utils/api-response";
 import { MESSAGE_DATA_DELETED, MESSAGE_INVALID_PARAMETER } from "../../../shared/constants/message.constant";
 import { ERROR_ON_DELETE } from "../../../shared/constants/error.constant";
 import UserPermissionService from "../../../services/user-permission.service";
+import BuildUserPermissionsService from "../../../services/rbac/build-user-permissions.service";
 import BadRequestException from "../../../shared/exceptions/bad-request.exception";
 
 const userPermissionService = new UserPermissionService();
@@ -14,18 +15,24 @@ const remove = async (
 ): Promise<void> => {
   try {
     const { params } = req;
-    const id = params.id;
+    const { id, userId } = params;
 
     if (id === ":id") {
       throw new BadRequestException([MESSAGE_INVALID_PARAMETER]);
     }
 
-    const newUserPermission = await userPermissionService.delete(id);
+    await userPermissionService.delete(id);
+
+    // Rebuild user permissions cache
+    const buildUserPermissionsService = new BuildUserPermissionsService({
+      userId: userId,
+      expireInMinutes: 30
+    });
+    await buildUserPermissionsService.execute();
 
     apiResponse(res, {
       statusCode: 200,
-      message: MESSAGE_DATA_DELETED,
-      data: newUserPermission
+      message: MESSAGE_DATA_DELETED
     });
   } catch (error) {
     console.error(`${ERROR_ON_DELETE}: `, error);

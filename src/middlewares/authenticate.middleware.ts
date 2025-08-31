16562a770fedc981f099c17387cae303a4a86be5
Christian Murillo <christianoroceomurillo@gmail.com>
2025-08-31 17:03:12 +0800

    Updated validation for authenticate

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import { MESSAGE_DATA_NOT_LOGGED, MESSAGE_INVALID_PARAMETER } from "../shared/constants/message.constant";
+import { MESSAGE_DATA_NOT_LOGGED } from "../shared/constants/message.constant";
 import AuthenticateService from "../services/authenticate.service";
 import UnauthorizedException from "../shared/exceptions/unauthorized.exception";
-import BadRequestException from "../shared/exceptions/bad-request.exception";
 
 const authenticate = async (
   req: Request,
@@ -16,10 +15,6 @@ const authenticate = async (
     if (!authorization) {
       throw new UnauthorizedException([MESSAGE_DATA_NOT_LOGGED]);
     };
-
-    if (organizationId === ":organizationId") {
-      throw new BadRequestException([MESSAGE_INVALID_PARAMETER]);
-    }
 
     const authenticateService = new AuthenticateService({
       token: authorization.split(" ")[1],

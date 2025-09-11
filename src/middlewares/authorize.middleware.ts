345c1d6b40bb25b3bb3f968919b9050766518d4f
Christian Murillo <christianoroceomurillo@gmail.com>
2025-09-11 17:28:21 +0800

    Updated authorize middleware

--- src/middlewares/authorize.middleware.ts
+++ src/middlewares/authorize.middleware.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import UnauthorizedException from "../shared/exceptions/unauthorized.exception";
 import { MESSAGE_DATA_NOT_AUTHORIZED } from "../shared/constants/message.constant";
 import redisConfig from "../config/redis.config";
-import BuildUserPermissionsService from "../services/rbac/build-user-permissions.service";
 
 const authorize = (action: string, resource: string) =>
   async (
@@ -13,25 +12,16 @@ const authorize = (action: string, resource: string) =>
     try {
       const authId = req.auth.id;
       const organizationId = req.auth.organizationId;
-
       if (!authId || !organizationId) {
-        throw new UnauthorizedException([MESSAGE_DATA_NOT_AUTHORIZED])
+        throw new UnauthorizedException([MESSAGE_DATA_NOT_AUTHORIZED]);
       }
 
       const cache = await redisConfig.get(`user_permissions:${authId}`);
-
-      let permissions: Record<string, string[]> = {};
-
-      if (cache) {
-        permissions = JSON.parse(cache);
-      } else {
-        const buildUserPermissionsService = new BuildUserPermissionsService({
-          userId: authId,
-          expireInMinutes: 30
-        });
-        await buildUserPermissionsService.execute();
+      if (!cache) {
+        throw new UnauthorizedException([MESSAGE_DATA_NOT_AUTHORIZED]);
       }
 
+      const permissions: Record<string, string[]> = JSON.parse(cache);
       if (!permissions || !permissions[resource] || !permissions[resource].includes(action)) {
         throw new UnauthorizedException([MESSAGE_DATA_NOT_AUTHORIZED]);
       }

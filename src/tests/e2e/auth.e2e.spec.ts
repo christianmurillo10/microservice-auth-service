81ea17a43263b0e4ad816e3f06d3d5889653fe5a
Christian Murillo <christianoroceomurillo@gmail.com>
2025-09-25 16:31:01 +0800

    Implemented full auth controller end to end test script

--- src/tests/e2e/auth.e2e.spec.ts
+++ src/tests/e2e/auth.e2e.spec.ts
@@ -1,22 +1,77 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeAll } from "vitest";
 import request from "supertest";
 import app from "../../app";
 
-describe("Auth Service", () => {
+describe("Auth - E2E", () => {
+  let header = {};
+  let refreshToken = "";
+
   it("should login seeded admin", async () => {
-    const res = await request(app).post("/auth/login").send({
-      email: "superadmin@email.com",
-      password: "password"
-    });
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "superadmin@email.com", password: "password" });
     expect(res.status).toBe(200);
     expect(res.body.data.token).toBeTruthy();
+
+    header = {
+      "authorization": `Bearer ${res.body.data.token}`
+    };
+    refreshToken = res.body.data.refreshToken;
   });
 
   it("should fail login with wrong credentials", async () => {
-    const res = await request(app).post("/auth/login").send({
-      email: "superadmin@email.com",
-      password: "wrong"
-    });
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "superadmin@email.com", password: "wrong" });
     expect(res.status).toBe(400);
+  });
+
+  it("should fail logout without header authorization", async () => {
+    const res = await request(app).post("/auth/logout");
+    expect(res.status).toBe(400);
+  });
+
+  it("should refresh token", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .set(header)
+      .send({ refreshToken });
+    expect(res.status).toBe(200);
+    expect(res.body.data.token).toBeTruthy();
+
+    header = {
+      "authorization": `Bearer ${res.body.data.token}`
+    };
+    refreshToken = res.body.data.refreshToken;
+  });
+
+  it("should fail refresh token without header authorization", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .send({ refreshToken });
+    expect(res.status).toBe(400);
+  });
+
+  it("should fail refresh token with empty refreshToken body", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .set(header)
+      .send({ refreshToken: "" });
+    expect(res.status).toBe(400);
+  });
+
+  it("should fail refresh token with not found refreshToken body", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .set(header)
+      .send({ refreshToken: "not_found" });
+    expect(res.status).toBe(404);
+  });
+
+  it("should logout", async () => {
+    const res = await request(app)
+      .post("/auth/logout")
+      .set(header);
+    expect(res.status).toBe(200);
   });
 });

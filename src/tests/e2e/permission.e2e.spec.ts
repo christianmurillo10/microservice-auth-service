e417b9a96b2076e0f6c4439694c40be26f2416b8
Christian Murillo <christianoroceomurillo@gmail.com>
2025-09-26 01:05:55 +0800

    Implemented end to end test scripts for permission module

---
+++ src/tests/e2e/permission.e2e.spec.ts
@@ -0,0 +1,79 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import request from "supertest";
+import app from "../../app";
+
+describe("Permission - E2E", () => {
+  const noutFoundId = "not-found-id";
+  let id = "";
+  let headers = {};
+
+  beforeAll(async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "superadmin@email.com", password: "password" });
+
+    headers = { "authorization": `Bearer ${res.body.data.token}` };
+  });
+
+  it("should create permission", async () => {
+    const res = await request(app)
+      .post("/permissions")
+      .set(headers)
+      .send({ action: "test-action", resource: "test-resource" });
+    expect(res.status).toBe(201);
+
+    id = res.body.data.id;
+  });
+
+  it("should fail create permission if action and resource exist", async () => {
+    const res = await request(app)
+      .post("/permissions")
+      .set(headers)
+      .send({ action: "test-action", resource: "test-resource" });
+    expect(res.status).toBe(409);
+  });
+
+  it("should update permission", async () => {
+    const res = await request(app)
+      .put(`/permissions/${id}`)
+      .set(headers)
+      .send({ action: "test-action", resource: "test-resource" });
+    expect(res.status).toBe(200);
+  });
+
+  it("should fail update permission if id not found", async () => {
+    const res = await request(app)
+      .put(`/permissions/${noutFoundId}`)
+      .set(headers)
+      .send({ action: "test-action", resource: "test-resource" });
+    expect(res.status).toBe(404);
+  });
+
+  it("should read permission", async () => {
+    const res = await request(app)
+      .get(`/permissions/${id}`)
+      .set(headers);
+    expect(res.status).toBe(200);
+  });
+
+  it("should fail read permission if id not found", async () => {
+    const res = await request(app)
+      .get(`/permissions/${noutFoundId}`)
+      .set(headers);
+    expect(res.status).toBe(404);
+  });
+
+  it("should list permissions", async () => {
+    const res = await request(app)
+      .get("/permissions")
+      .set(headers);
+    expect(res.status).toBe(200);
+  });
+
+  it("should delete permission", async () => {
+    const res = await request(app)
+      .delete(`/permissions/${id}`)
+      .set(headers);
+    expect(res.status).toBe(200);
+  });
+});

8b3faf5dcfe60144f2e8ff781203836312c49917
Christian Murillo <christianoroceomurillo@gmail.com>
2025-09-26 00:54:07 +0800

    Updated organization test scripts

--- src/tests/e2e/organization.e2e.spec.ts
+++ src/tests/e2e/organization.e2e.spec.ts
@@ -3,8 +3,9 @@ import request from "supertest";
 import app from "../../app";
 
 describe("Auth - E2E", () => {
-  let headers = {};
+  const noutFoundId = "not-found-id";
   let id = "";
+  let headers = {};
 
   beforeAll(async () => {
     const res = await request(app)
@@ -24,6 +25,14 @@ describe("Auth - E2E", () => {
     id = res.body.data.id;
   });
 
+  it("should fail create organization if name exist", async () => {
+    const res = await request(app)
+      .post("/organizations")
+      .set(headers)
+      .send({ name: "Test Organization" });
+    expect(res.status).toBe(409);
+  });
+
   it("should update organization", async () => {
     const res = await request(app)
       .put(`/organizations/${id}`)
@@ -32,11 +41,26 @@ describe("Auth - E2E", () => {
     expect(res.status).toBe(200);
   });
 
+  it("should fail update organization if id not found", async () => {
+    const res = await request(app)
+      .put(`/organizations/${noutFoundId}`)
+      .set(headers)
+      .send({ name: "Test Organization - updated" });
+    expect(res.status).toBe(404);
+  });
+
   it("should read organization", async () => {
     const res = await request(app)
       .get(`/organizations/${id}`)
       .set(headers);
     expect(res.status).toBe(200);
+  });
+
+  it("should fail read organization if id not found", async () => {
+    const res = await request(app)
+      .get(`/organizations/${noutFoundId}`)
+      .set(headers);
+    expect(res.status).toBe(404);
   });
 
   it("should list organizations", async () => {

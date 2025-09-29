557892e6b06b3b134039208bd1bbd3fc97719d1d
Christian Murillo <christianoroceomurillo@gmail.com>
2025-09-29 23:11:27 +0800

    Initial comimt for auth integration test script

---
+++ src/tests/integration/auth.integration.spec.ts
@@ -0,0 +1,73 @@
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+import app from "../../app";
+
+let headers = {};
+let refreshToken = "";
+
+describe("Auth - Integration", () => {
+  it("should login seeded admin", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "superadmin@email.com", password: "password" });
+    expect(res.status).toBe(200);
+    expect(res.body.data.token).toBeTruthy();
+
+    headers = { "authorization": `Bearer ${res.body.data.token}` };
+    refreshToken = res.body.data.refreshToken;
+  });
+
+  it("should fail login with wrong credentials", async () => {
+    const res = await request(app)
+      .post("/auth/login")
+      .send({ email: "superadmin@email.com", password: "wrong" });
+    expect(res.status).toBe(400);
+  });
+
+  it("should fail logout without headers authorization", async () => {
+    const res = await request(app).post("/auth/logout");
+    expect(res.status).toBe(400);
+  });
+
+  it("should refresh token", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .set(headers)
+      .send({ refreshToken });
+    expect(res.status).toBe(200);
+    expect(res.body.data.token).toBeTruthy();
+
+    headers = { "authorization": `Bearer ${res.body.data.token}` };
+    refreshToken = res.body.data.refreshToken;
+  });
+
+  it("should fail refresh token without headers authorization", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .send({ refreshToken });
+    expect(res.status).toBe(400);
+  });
+
+  it("should fail refresh token with empty refreshToken body", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .set(headers)
+      .send({ refreshToken: "" });
+    expect(res.status).toBe(400);
+  });
+
+  it("should fail refresh token with not found refreshToken body", async () => {
+    const res = await request(app)
+      .post("/auth/refresh-token")
+      .set(headers)
+      .send({ refreshToken: "not_found" });
+    expect(res.status).toBe(404);
+  });
+
+  it("should logout", async () => {
+    const res = await request(app)
+      .post("/auth/logout")
+      .set(headers);
+    expect(res.status).toBe(200);
+  });
+});

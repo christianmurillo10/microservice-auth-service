f581054a3f189ad4dc2a7d57f51b80520e7c5cc2
Christian Murillo <christianoroceomurillo@gmail.com>
2025-10-03 20:18:39 +0800

    Implemented unit test for jwt entity

---
+++ src/tests/unit/entities/jwt.entity.spec.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect } from "vitest";
+import { faker } from "@faker-js/faker";
+import JWTEntity from "../../../entities/jwt.entity";
+import { UserAccessType } from "../../../entities/user.entity";
+
+let token: string;
+
+describe("JWT Entity", () => {
+  it("should encode jwt token", async () => {
+    const jwt = new JWTEntity({
+      id: faker.number.int(),
+      email: faker.internet.email(),
+      client: UserAccessType.Portal,
+      scope: "*",
+      sub: faker.number.int(),
+      exp: Date.now() + 1000 * 60 * 60,
+      iat: Date.now() / 1000,
+      aud: "Microservice"
+    });
+    token = jwt.encodeToken();
+    expect(token).toBeTypeOf("string");
+  });
+
+  it("should decode jwt token", async () => {
+    const decoded = JWTEntity.decodeToken(token);
+    expect(decoded).toHaveProperty("id");
+    expect(decoded).toHaveProperty("email");
+    expect(decoded).toHaveProperty("client");
+    expect(decoded).toHaveProperty("scope");
+    expect(decoded).toHaveProperty("sub");
+    expect(decoded).toHaveProperty("exp");
+    expect(decoded).toHaveProperty("iat");
+    expect(decoded).toHaveProperty("aud");
+  });
+});

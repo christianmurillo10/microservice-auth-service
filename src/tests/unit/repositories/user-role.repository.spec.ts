1a03cbd8ca2f59252d1bde878f0ba0c6d8446024
Christian Murillo <christianoroceomurillo@gmail.com>
2025-10-15 17:31:55 +0800

    Updated unit test scripts for user role

--- src/tests/unit/repositories/user-role.repository.spec.ts
+++ src/tests/unit/repositories/user-role.repository.spec.ts
@@ -30,25 +30,41 @@ describe("User Role Repository - Unit", () => {
     const result = await repo.create({
       params: new UserRoleEntity(basedata)
     });
-    expect(prisma.userRole.create).toHaveBeenCalled();
+
+    expect(prisma.userRole.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        data: expect.objectContaining({
+          roleId: basedata.roleId,
+        }),
+      }),
+    );
     expect(result.roleId).toBe(basedata.roleId);
   });
 
   it("should return all user roles", async () => {
     const result = await repo.findAll({});
+
     expect(prisma.userRole.findMany).toHaveBeenCalled();
+    expect(result).toBeInstanceOf(Array);
     expect(result.length).toBeGreaterThan(0);
   });
 
   it("should return all user roles by userId", async () => {
     const result = await repo.findAllByUserId({ userId: basedata.userId });
+    
     expect(prisma.userRole.findMany).toHaveBeenCalled();
+    expect(result).toBeInstanceOf(Array);
     expect(result.length).toBeGreaterThan(0);
   });
 
   it("should find user role by id", async () => {
     const result = await repo.findById({ id: basedata.id });
-    expect(prisma.userRole.findFirst).toHaveBeenCalled();
+
+    expect(prisma.userRole.findFirst).toHaveBeenCalledWith(
+      expect.objectContaining({
+        where: expect.objectContaining({ id: basedata.id }),
+      }),
+    );
     expect(result?.id).toBe(basedata.id);
   });
 
@@ -57,18 +73,28 @@ describe("User Role Repository - Unit", () => {
       userId: basedata.userId,
       roleId: basedata.roleId
     });
-    expect(prisma.userRole.findFirst).toHaveBeenCalled();
+
+    expect(prisma.userRole.findFirst).toHaveBeenCalledWith(
+      expect.objectContaining({
+        where: expect.objectContaining({
+          userId: basedata.userId,
+          roleId: basedata.roleId
+        }),
+      }),
+    );
     expect(result?.userId).toBe(basedata.userId);
     expect(result?.roleId).toBe(basedata.roleId);
   });
 
   it("should delete user role", async () => {
     await repo.delete({ id: basedata.id });
+
     expect(prisma.userRole.delete).toHaveBeenCalled();
   });
 
   it("should count user roles", async () => {
     const result = await repo.count();
+    
     expect(prisma.userRole.count).toHaveBeenCalled();
     expect(result).toBeGreaterThan(0);
   });
